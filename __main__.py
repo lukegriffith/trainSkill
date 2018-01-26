2177d447ee30e567d320d24e448585aacc4e5754
lukemgriffith <lukemgriffith@gmail.com>
2018-01-26 00:18:48 +0000

    removed whitespace

--- __main__.py
+++ __main__.py
@@ -2,12 +2,8 @@
 from trainSkill.NationalRail import NrClient
 from trainSkill.skill import DepartureSkill
 
-
 TOKEN = '78857c37-7ed6-4b13-bfde-bc5409bc3de8'
 URI = 'https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2017-10-01'
-
-
-
 NR = NrClient(TOKEN, URI)
 
 DepartureSkill(NR, 'SNS', 'WAT', 30)
